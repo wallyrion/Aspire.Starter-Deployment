ace47e390087217c9d162659ff60305e620571c7
Oleksii Korniienko <kornienko1296@gmail.com>
2024-08-07 20:53:23 +0300

    Configure azure resources

--- AspireSample.AppHost/Program.cs
+++ AspireSample.AppHost/Program.cs
@@ -1,6 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
+/*builder.AddRedis("redis").PublishAsAzureRedis((_, _, cache) =>
+{
+    var (sku, family) = builder.Environment.EnvironmentName switch
+    {
+        "Staging" => ("Standard", "C0"),
+        "Live" => ("Premium", "C1"),
+        _ => ("Basic", "C0")
+    };
+
+    cache.AssignProperty(r => r.s);
+    cache.AssignProperty(r => r.Sku.Name, sku);
+    cache.AssignProperty(r => r.Sku.Family, family);
+});*/
 
 var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice");
 
